812564eec803394d6870aab6dab69bbf87add553
Catgirl <zeroaninea@gmail.com>
2026-02-23 21:45:32 +0400

    Prisma connected

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,21 @@
+// import { Injectable } from '@nestjs/common';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from '../../src/generated/prisma/client';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient {
+//   constructor() {
+//     const adapter = new PrismaPg({
+//       connectionString: process.env.DATABASE_URL as string,
+//     });
+
+//     super({ adapter });
+//   }
+// }
+
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../src/generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
