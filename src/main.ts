cdc48150617be60bdf37bdfffb15e0033909a9d6
Catgirl <zeroaninea@gmail.com>
2026-02-23 21:55:59 +0400

    configservice

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap()
-  .then(() => console.log(` 🚀 Server runs on port ${process.env.PORT}`))
+  .then(() => console.log(` 🚀 Server runs on port ${process.env.PORT}...`))
   .catch((err) => console.log(err));
