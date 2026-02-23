3e15b584168a3f9cd413d8b7a9ab2829914f0902
Catgirl <zeroaninea@gmail.com>
2026-02-23 19:24:11 +0400

    hello world works

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() =>
+    console.log(` 🚀 Server started on port ${process.env.PORT ?? 3000}...`),
+  )
+  .catch(console.error);
