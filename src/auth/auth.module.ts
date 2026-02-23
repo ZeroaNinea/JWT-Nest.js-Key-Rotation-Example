844a119d6e94e12548400bcb90caf139ab9e55dc
Catgirl <zeroaninea@gmail.com>
2026-02-24 03:36:34 +0400

    JWT imported

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthController } from './auth.controller';
 
 import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory(configService: ConfigService) {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '15m' },
+        };
+      },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AuthController],
   providers: [UsersService, PrismaService],
 })
