fdfa35bd1fd5b69804604df76dae0a9cfd953275
Catgirl <zeroaninea@gmail.com>
2026-02-24 21:05:59 +0400

    the key rotation integrated

--- src/auth/key-store.service.ts
+++ src/auth/key-store.service.ts
@@ -5,27 +5,57 @@ import * as path from 'path';
 
 @Injectable()
 export class KeyStoreService {
-  private keys: { [key: string]: { privateKey: Buffer; publicKey: Buffer } } = {
-    '1': {
-      privateKey: fs.readFileSync(
-        path.join(__dirname, '../../../keys/private_1.pem'),
-      ),
-      publicKey: fs.readFileSync(
-        path.join(__dirname, '../../../keys/public_1.pem'),
-      ),
-    },
-  };
+  // private keys: { [key: string]: { privateKey: Buffer; publicKey: Buffer } } = {
+  //   '1': {
+  //     privateKey: fs.readFileSync(
+  //       path.join(__dirname, '../../../keys/private_1.pem'),
+  //     ),
+  //     publicKey: fs.readFileSync(
+  //       path.join(__dirname, '../../../keys/public_1.pem'),
+  //     ),
+  //   },
+  // };
 
-  private currentKid = '1';
+  // private currentKid = '1';
+
+  // getCurrentPrivateKey() {
+  //   return {
+  //     kid: this.currentKid,
+  //     privateKey: this.keys[this.currentKid].privateKey,
+  //   };
+  // }
+
+  // getPublicKey(kid: string) {
+  //   return this.keys[kid]?.publicKey;
+  // }
+
+  private keysDir = path.resolve(process.cwd(), 'dist/keys');
 
   getCurrentPrivateKey() {
-    return {
-      kid: this.currentKid,
-      privateKey: this.keys[this.currentKid].privateKey,
-    };
+    const keyMap = this.loadKeyMap();
+    const kids = Object.keys(keyMap).sort();
+    const currentKid = kids.at(-1)!;
+
+    const privateKey = fs.readFileSync(
+      path.join(this.keysDir, `${currentKid}.private.pem`),
+    );
+
+    return { kid: currentKid, privateKey };
   }
 
   getPublicKey(kid: string) {
-    return this.keys[kid]?.publicKey;
+    const publicPath = path.join(this.keysDir, `${kid}.public.pem`);
+
+    if (!fs.existsSync(publicPath)) return null;
+
+    return fs.readFileSync(publicPath);
+  }
+
+  private loadKeyMap() {
+    const keyMapPath = path.join(this.keysDir, 'key-map.json');
+    return JSON.parse(fs.readFileSync(keyMapPath, 'utf-8')) as Record<
+      string,
+      string
+    >;
   }
 }
