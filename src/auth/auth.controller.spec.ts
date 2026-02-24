f63bcae05050cfd0a28768ab453f4e0e1f5b67af
Catgirl <zeroaninea@gmail.com>
2026-02-25 00:49:27 +0400

    all tests passed

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { KeyRotationService } from './key-rotation.service';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthService } from './auth.service';
+import { KeyStoreService } from './key-store.service';
+import { JwtStrategy } from './jwt.strategy';
+
 import { AuthController } from './auth.controller';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +17,16 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        KeyRotationService,
+        UsersService,
+        PrismaService,
+        AuthService,
+        KeyStoreService,
+        JwtStrategy,
+        ConfigService,
+        JwtService,
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,4 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+import { KeyRotationService } from './key-rotation.service';
+import { UsersService } from '../users/users.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { KeyStoreService } from './key-store.service';
+import { JwtStrategy } from './jwt.strategy';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,7 +15,16 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        KeyRotationService,
+        UsersService,
+        PrismaService,
+        AuthService,
+        KeyStoreService,
+        JwtStrategy,
+        ConfigService,
+        JwtService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
