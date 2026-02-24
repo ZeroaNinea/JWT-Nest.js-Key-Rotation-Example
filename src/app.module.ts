1f3a7a100b6ce82587874ef1c4086dfd597688a1
Catgirl <zeroaninea@gmail.com>
2026-02-24 14:17:53 +0400

    key-store.service.ts created

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { KeyStoreService } from './auth/key-store.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, KeyStoreService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthService } from './auth.service';
+import { KeyStoreService } from './key-store.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
@@ -26,6 +27,12 @@ import { JwtStrategy } from './jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [UsersService, PrismaService, AuthService, JwtStrategy],
+  providers: [
+    UsersService,
+    PrismaService,
+    AuthService,
+    KeyStoreService,
+    JwtStrategy,
+  ],
 })
 export class AuthModule {}
