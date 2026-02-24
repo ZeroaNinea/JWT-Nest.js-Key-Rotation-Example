7970e4f983f48bdf3e158a0e141ce04bb7c98330
Catgirl <zeroaninea@gmail.com>
2026-02-24 14:33:46 +0400

    the getCurrentPrivateKey and getPublicKey methods added

--- src/auth/key-store.service.ts
+++ src/auth/key-store.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
 
+import * as fs from 'fs';
+import * as path from 'path';
+
 @Injectable()
-export class KeyStoreService {}
+export class KeyStoreService {
+  private keys: { [key: string]: { privateKey: Buffer; publicKey: Buffer } } = {
+    '1': {
+      privateKey: fs.readFileSync(path.join(__dirname, 'keys/private_1.pem')),
+      publicKey: fs.readFileSync(path.join(__dirname, 'keys/public_1.pem')),
+    },
+  };
+
+  private currentKid = '1';
+
+  getCurrentPrivateKey() {
+    return {
+      kid: this.currentKid,
+      privateKey: this.keys[this.currentKid].privateKey,
+    };
+  }
+
+  getPublicKey(kid: string) {
+    return this.keys[kid]?.publicKey;
+  }
+}
