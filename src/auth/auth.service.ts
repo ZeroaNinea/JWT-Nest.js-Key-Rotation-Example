7fbbf059eae45bcd178d039ea4180af51b1f9baa
Catgirl <zeroaninea@gmail.com>
2026-02-24 15:00:47 +0400

    auth.service.ts updated

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,13 +3,22 @@ import { JwtService } from '@nestjs/jwt';
 
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/login.dto';
+import { KeyStoreService } from './key-store.service';
 
 @Injectable()
 export class AuthService {
+  private kid: string;
+  private privateKey: Buffer;
+
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
-  ) {}
+    private keyStore: KeyStoreService,
+  ) {
+    const { kid, privateKey } = this.keyStore.getCurrentPrivateKey();
+    this.kid = kid;
+    this.privateKey = privateKey;
+  }
 
   async login(dto: LoginDto) {
     const user = await this.usersService.validateUser(dto);
@@ -21,7 +30,15 @@ export class AuthService {
     const payload = { sub: user.id, email: user.email };
 
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: await this.jwtService.signAsync(payload, {
+        privateKey: this.privateKey,
+        algorithm: 'RS256',
+        header: {
+          alg: 'RS256',
+          kid: this.kid,
+        },
+        expiresIn: '15m',
+      }),
     };
   }
 }
